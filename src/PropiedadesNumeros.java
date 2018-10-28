1e96d0f36167c90233d507398a1a1ae510f6c88b
Pilar Melió <mariap.melio@gmail.com>
2018-10-28 12:25:41 +0100

    inicial

---
+++ src/PropiedadesNumeros.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author maria
+ */
+public class PropiedadesNumeros {
+    public boolean esPar(int n){
+        return n%2==0;
+    }
+}
