b75295c10b7810afc6d5da4ea2512aac866abd91
Pilar Melió <mariap.melio@gmail.com>
2018-10-28 12:40:33 +0100

    nueo test

--- test/PropiedadesNumerosTest.java
+++ test/PropiedadesNumerosTest.java
@@ -23,7 +23,7 @@ public class PropiedadesNumerosTest {
     @Test
     public void testEsPar() {
         System.out.println("esPar");
-        int n = 3;
+        int n = 2;
         PropiedadesNumeros instance = new PropiedadesNumeros();
         boolean expResult = false;
         boolean result = instance.esPar(n);
